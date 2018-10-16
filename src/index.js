7d4e095b495595cec8fc7435dc6727639488bb53
Jonathan Wagner <jonathanw@theodo.co.uk>
2018-10-16 11:08:28 +0100

    Add timer and debug tools

--- src/index.js
+++ src/index.js
@@ -10,3 +10,21 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+console.log("Tak...");
+let interval = setInterval(tik, 1000);
+setTimeout(boom, 3000);
+
+function tik() {
+  console.log("Tik...");
+}
+
+function boom() {
+  console.log("Boom");
+  clearInterval(interval);
+}
+
+let a = 2;
+let b = 4;
+let c = a + b;
+console.log(c);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  mode: "production",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "./dist"
   }
 };
